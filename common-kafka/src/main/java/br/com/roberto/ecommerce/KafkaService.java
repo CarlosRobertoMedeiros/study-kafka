555e391f2e56eeaff71b1217786326572d6f678f
Carlos Roberto <carlosmedeiroslima@gmail.com>
2021-09-15 09:40:50 -0300

    Atualizando os Serviços

--- common-kafka/src/main/java/br/com/roberto/ecommerce/KafkaService.java
+++ common-kafka/src/main/java/br/com/roberto/ecommerce/KafkaService.java
@@ -15,7 +15,7 @@ import org.apache.kafka.common.serialization.StringDeserializer;
 class KafkaService<T> implements Closeable {
 
 	private final KafkaConsumer<String, Message<T>> consumer;
-	private final ConsumerFunction parse;
+	private final ConsumerFunction<T> parse;
 
 	KafkaService(String groupId, String topic, ConsumerFunction<T> parse, Class<T> type, Map<String, String> properties) {
 		this(parse,groupId, type, properties);
@@ -27,7 +27,7 @@ class KafkaService<T> implements Closeable {
 		consumer.subscribe(topic);
 	}
 	
-	private KafkaService(ConsumerFunction parse, String groupId, Class<T> type, Map<String, String> properties) {
+	private KafkaService(ConsumerFunction<T> parse, String groupId, Class<T> type, Map<String, String> properties) {
 		this.parse = parse;
 		this.consumer = new KafkaConsumer<>(getProperties(type, groupId, properties));
 	}
--- service-log/src/main/java/br/com/roberto/ecommerce/LogService.java
+++ service-log/src/main/java/br/com/roberto/ecommerce/LogService.java
@@ -12,7 +12,7 @@ public class LogService {
 	public static void main(String[] args) {
 		
 		var logService = new LogService();
-		try (var service = new KafkaService(LogService.class.getSimpleName(),
+		try (var service = new KafkaService<>(LogService.class.getSimpleName(),
 				Pattern.compile("ECOMMERCE.*"),
 				logService::parse,
 				String.class,
